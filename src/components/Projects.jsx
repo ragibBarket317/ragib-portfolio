cb62bf43e4bfca830fc0324f315e7628967c9df9
Ragib Barket <ragibrahman86317@gmail.com>
2024-12-27 21:42:31 +0600

    add animation on project card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,13 +9,14 @@ const Projects = () => {
           I have worked on a wide range of projects. Here are some of my
           projects.
         </p>
-        <div className="mt-10  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 lg:gap-4 items-center">
+        <div className="mt-10  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 lg:gap-8 items-center">
           <motion.div
             initial={{ y: 100, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
             viewport={{ once: true, amount: 0.2 }}
-            transition={{ duration: 0.4 }}
-            className="bg-[#212428]  h-[480px] rounded p-5 shadow-custom"
+            transition={{ duration: 0.6 }}
+            whileHover={{ scale: 1.05 }}
+            className="bg-[#212428]  h-[480px]  rounded p-5 shadow-custom"
           >
             <div className="h-[200px] rounded border">
               <img src="/cloth_store.png" className="w-full h-full" alt="" />
@@ -40,26 +41,33 @@ const Projects = () => {
               </span>
             </div>
             <div className="flex justify-self-end mt-8">
-              <button className="ml-2 text-[14px] font-semibold bg-purple-800 py-2 px-4 rounded">
+              <motion.button
+                whileHover={{ scale: 1.05 }}
+                className="ml-2 text-[14px] font-semibold bg-purple-800 py-2 px-4 rounded"
+              >
                 <a
                   href="https://github.com/ragibBarket317/cloth-store"
                   target="_blank"
                 >
                   Code
                 </a>
-              </button>
-              <button className="ml-2 text-[14px] font-semibold bg-purple-800 py-2 px-4 rounded">
+              </motion.button>
+              <motion.button
+                whileHover={{ scale: 1.05 }}
+                className="ml-2 text-[14px] font-semibold bg-purple-800 py-2 px-4 rounded"
+              >
                 <a href="https://cloth-store-smoky.vercel.app" target="_blank">
                   Live Preview
                 </a>
-              </button>
+              </motion.button>
             </div>
           </motion.div>
           <motion.div
             initial={{ y: 100, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
             viewport={{ once: true, amount: 0.2 }}
-            transition={{ duration: 0.6 }}
+            transition={{ duration: 0.8 }}
+            whileHover={{ scale: 1.05 }}
             className="bg-[#212428]  h-[480px] rounded p-5 shadow-custom"
           >
             <div className="h-[200px] rounded border">
@@ -89,29 +97,36 @@ const Projects = () => {
               </span>
             </div>
             <div className="flex justify-self-end mt-8">
-              <button className="ml-2 text-[14px] font-semibold bg-purple-800 py-2 px-4 rounded">
+              <motion.button
+                whileHover={{ scale: 1.05 }}
+                className="ml-2 text-[14px] font-semibold bg-purple-800 py-2 px-4 rounded"
+              >
                 <a
                   href="https://github.com/ragibBarket317/cloth-store"
                   target="_blank"
                 >
                   Code
                 </a>
-              </button>
-              <button className="ml-2 text-[14px] font-semibold bg-purple-800 py-2 px-4 rounded">
+              </motion.button>
+              <motion.button
+                whileHover={{ scale: 1.05 }}
+                className="ml-2 text-[14px] font-semibold bg-purple-800 py-2 px-4 rounded"
+              >
                 <a
                   href="https://cloth-store-admin-nine.vercel.app"
                   target="_blank"
                 >
                   Live Preview
                 </a>
-              </button>
+              </motion.button>
             </div>
           </motion.div>
           <motion.div
             initial={{ y: 100, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
             viewport={{ once: true, amount: 0.2 }}
-            transition={{ duration: 0.8 }}
+            transition={{ duration: 1 }}
+            whileHover={{ scale: 1.05 }}
             className="bg-[#212428]  h-[480px] rounded p-5 shadow-custom"
           >
             <div className="h-[200px] rounded border">
@@ -131,22 +146,28 @@ const Projects = () => {
               </span>
             </div>
             <div className="flex justify-self-end mt-8">
-              <button className="ml-2 text-[14px] font-semibold bg-purple-800 py-2 px-4 rounded">
+              <motion.button
+                whileHover={{ scale: 1.05 }}
+                className="ml-2 text-[14px] font-semibold bg-purple-800 py-2 px-4 rounded"
+              >
                 <a
                   href="https://github.com/ragibBarket317/weather-dashboard"
                   target="_blank"
                 >
                   Code
                 </a>
-              </button>
-              <button className="ml-2 text-[14px] font-semibold bg-purple-800 py-2 px-4 rounded">
+              </motion.button>
+              <motion.button
+                whileHover={{ scale: 1.05 }}
+                className="ml-2 text-[14px] font-semibold bg-purple-800 py-2 px-4 rounded"
+              >
                 <a
                   href="https://weather-dashboard-omega.vercel.app"
                   target="_blank"
                 >
                   Live Preview
                 </a>
-              </button>
+              </motion.button>
             </div>
           </motion.div>
         </div>
